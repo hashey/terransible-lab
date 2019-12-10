ee14df5a3b256a4d4339d588a9e41a80529ef916
hashey <justin.kiley@gmail.com>
2019-12-09 20:27:16 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+#this file is used to declare variables and initial default values
+
 variable "aws_region" {}
 variable "aws_profile" {}
 variable "vpc_cidr" {}
@@ -11,3 +13,18 @@ variable "db_instance_class" {}
 variable "dbname" {}
 variable "dbuser" {}
 variable "dbpassword" {}
+variable "dev_instance_type" {}
+variable "dev_ami" {}
+variable "public_key_path" {}
+variable "key_name" {}
+variable "elb_healthy_threshold" {}
+variable "elb_unhealthy_threshold" {}
+variable "elb_timeout" {}
+variable "elb_interval" {}
+variable lc_instance_type {}
+variable asg_max {}
+variable asg_min {}
+variable asg_grace {}
+variable asg_hct {}
+variable asg_cap {}
+variable delegation_set_id {}
